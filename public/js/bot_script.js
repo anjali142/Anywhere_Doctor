7ca8db405ad6a3faaf258654627963a78811229c
khyati18 <agarwal.khyati5@gmail.com>
2020-12-09 12:16:32 +0530

    bot update

--- public/js/bot_script.js
+++ public/js/bot_script.js
@@ -1,3 +1,13 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+// Icons made by Freepik from www.flaticon.com
+const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "USER";
+
 const btn = document.querySelector("button");
 const outputme = document.querySelector(".output-you");
 const outputbot = document.querySelector(".output-bot");
@@ -20,7 +30,8 @@ recognition.onresult = function (event) {
   const text = event.results[last][0].transcript;
   console.log(text);
 
-  outputme.textContent = text;
+  // outputme.textContent = text;
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", text);
 
   socket.emit("chat message", text);
 };
@@ -35,6 +46,55 @@ const botReply = (text) => {
 };
 
 socket.on("bot reply", (text) => {
-  outputbot.textContent = text;
+  // outputbot.textContent = text;
+  appendMessage(BOT_NAME, BOT_IMG, "left", text);
   botReply(text);
 });
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  socket.emit("chat message", msgText);
+
+  msgerInput.value = "";
+});
+
+function appendMessage(name, img, side, text) {
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
